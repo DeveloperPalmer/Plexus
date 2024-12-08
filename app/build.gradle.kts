79fc418ad788851ad0922c0a694b12e7b3f49a4e
Sergey Lanovoy <sla@kode.ru>
2024-12-08 18:12:29 +0200

    Add compose main activity
    (cherry picked from commit 857a3b9c6337e33e5f64627cc86d3e24b9b2a72d)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,6 +22,9 @@ android {
     versionName = "1.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    vectorDrawables {
+      useSupportLibrary = true
+    }
   }
 
   buildTypes {
@@ -37,6 +40,17 @@ android {
   kotlinOptions {
     jvmTarget = "1.8"
   }
+  buildFeatures {
+    compose = true
+  }
+  composeOptions {
+    kotlinCompilerExtensionVersion = "1.5.1"
+  }
+  packaging {
+    resources {
+      excludes += "/META-INF/{AL2.0,LGPL2.1}"
+    }
+  }
 }
 
 dependencies {
@@ -49,6 +63,19 @@ dependencies {
   implementation(libs.firebase.config)
   implementation(libs.sqldelight.android.driver)
   api(libs.sqldelight.coroutines)
+
+  implementation(platform(libs.androidx.compose.bom))
+  androidTestImplementation(platform(libs.androidx.compose.bom))
+
+  implementation(libs.androidx.lifecycle.runtime.ktx)
+  implementation(libs.androidx.activity.compose)
+  implementation(libs.androidx.ui)
+  implementation(libs.androidx.ui.graphics)
+  implementation(libs.androidx.ui.tooling.preview)
+  implementation(libs.androidx.material3)
+  androidTestImplementation(libs.androidx.ui.test.junit4)
+  debugImplementation(libs.androidx.ui.tooling)
+  debugImplementation(libs.androidx.ui.test.manifest)
 }
 
 detekt {
