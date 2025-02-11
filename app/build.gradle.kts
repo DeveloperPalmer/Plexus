7e89a77c945d52d3d54968fac6a5ecda73629031
Sergey Lanovoy <sla@kode>
2025-02-11 11:38:23 +0200

    Add detekt rules

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,12 @@
+import io.gitlab.arturbosch.detekt.Detekt
+import io.gitlab.arturbosch.detekt.DetektCreateBaselineTask
+
 plugins {
   alias(libs.plugins.android.application)
   alias(libs.plugins.jetbrains.kotlin.android)
   alias(libs.plugins.sqldelight)
+  alias(libs.plugins.spotless)
+  alias(libs.plugins.detekt)
 }
 
 android {
@@ -33,19 +38,38 @@ android {
   }
 }
 
-sqldelight {
-  databases {
-    create("FeatureDB") {
-      packageName = "ru.kode.plexus.database"
-      srcDirs("src/main/sqldelight/feature")
-    }
-  }
-}
-
 dependencies {
+  detektPlugins(files(rootProject.file("libs/detekt-rules-1.4.0.jar")))
+  detektPlugins(libs.detekt.rules.compose)
+
   implementation(libs.androidx.core.ktx)
   implementation(libs.material)
 
   implementation(libs.sqldelight.android.driver)
   api(libs.sqldelight.coroutines)
+}
+
+detekt {
+  config.setFrom(rootProject.file("detekt-config.yml"))
+  ignoredBuildTypes = listOf("release")
+}
+
+tasks.withType<Detekt>().configureEach {
+  jvmTarget = "11"
+  // "exclude(**/generated/**)" doesn't work for some reason, see
+  // https://github.com/detekt/detekt/issues/4127#issuecomment-1260733842
+  exclude { element -> // Use separator to make sure it also works on Windows
+    val sep = File.separator
+    element.file.absolutePath.contains("${sep}generated${sep}")
+  }
+  reports {
+    xml.required.set(false)
+    html.required.set(false)
+    txt.required.set(false)
+    sarif.required.set(false)
+  }
+}
+
+tasks.withType<DetektCreateBaselineTask>().configureEach {
+  jvmTarget = "11"
 }
